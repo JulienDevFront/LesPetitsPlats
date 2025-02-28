3eb3401dd39a3905e0f0b0cd2ada32d5d5cdc851
--global <mp.dev.mg@gmail.com>
2025-02-28 23:18:56 +0100

    Add feature generate dynamic "recipe card" in the DOM

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,27 @@
 import { promise } from "./helpers/promise.js";
 import { API_repices } from "./services/API_repices.js";
-//
 import { recipesCountSection } from "./view/recipesCountView.js";
-
-
+import { ConstructorRecipeCard } from "./models/contructorRecipeCard.js";
 
 const App = async () => {
     const API_repices_path = "https://juliendevfront.github.io/LesPetitsPlats/public/API_recipes.json";
     const API_repices_import = await promise(`${API_repices_path}`);
     const API_repices_instance = new API_repices(API_repices_import);
-    console.log(API_repices_instance);
+    const API_repices_data = API_repices_instance.data;
+    console.log(API_repices_data);
 
+    const recipesContainer = document.querySelector("#recipesSection");
+
+    if (API_repices_data && API_repices_data.length > 0) {
+        API_repices_data.forEach(recipe => {
+            const recipeCard = new ConstructorRecipeCard(recipe);
+            recipeCard.createRecipeCard();
+        });
+    } else {
+        recipesContainer.innerHTML = "<p>Aucune recette trouvée.</p>";
+    }
 
     recipesCountSection();
-
 };
 
-App(); 
+App();
---
+++ src/scripts/models/contructorRecipeCard.js
@@ -0,0 +1,47 @@
+/** JS.DOC
+ * - - -
+ * @module and @class ConstructorRecipeCard
+ * - - -
+ * @description This class  is responsible for dynamically generating recipe cards in the DOM.
+ * Each recipe card displays an image, a title, the preparation time, a brief description, and 
+ * a list of ingredients. The generated card is inserted into the "#recipesSection" container.
+ * - - -
+ */
+export class ConstructorRecipeCard {
+    constructor({id, image, name, servings, ingredients, time, description}) {
+        this._id = id;
+        this._image = image;
+        this._name = name;
+        this._servings = servings;
+        this._ingredients = ingredients;
+        this._time = time;
+        this._description = description;
+    }     
+
+    createRecipeCard() {
+        const container = document.querySelector('#recipesSection');
+        const content = `
+            <article class="w-80 rounded-3xl bg-white" name="${this._id}">
+                <header class="relative">
+                    <img class="h-64 object-cover" src="./assets/images/${this._image}" alt="${this._name}"/>
+                    <h3 class="pt-8 pl-6 pr-6 font-anton text-lg text-black">${this._name}</h3>
+                    <p class="font-manrope text-xs text-black font-normal bg-yellow-300 w-16 flex justify-center content-center rounded-xl absolute top-5 right-5">${this._time}mn</p>
+                </header>
+                <section class="pt-7 pl-6 pr-6">
+                    <h4 class="font-manrope font-bold text-gray-400 text-xs">Recette</h4>
+                    <p class="pt-4 text-black font-manrope font-normal text-sm">${this._description}</p>
+                    <h4 class="pt-8 font-manrope font-bold text-gray-400 text-xs">Ingrédients</h4>
+                    <ul class="text-sm font-manrope grid grid-cols-2 pb-16 pt-4 gap-5">
+                        ${this._ingredients.map(ingredient => `
+                            <li>
+                                <span class="text-black text-sm flex flex-col">${ingredient.ingredient}</span>
+                                <span class="text-gray-400 text-sm">${ingredient.quantity ? ingredient.quantity : "" }${ingredient.unit ? ingredient.unit : "" }</span>
+                            </li>`
+                        ).join("")}
+                    </ul>
+                </section>
+            </article>
+      `;
+    container.innerHTML += content;
+    }
+}
