56b3c8a7e2e874af6cf6429b48e73ec2381bc86b
--global <mp.dev.mg@gmail.com>
2025-04-02 22:34:13 +0200

    Re-factoring of data_ApiSingleton.js

---
+++ src/scripts/data/service_ApiManager.js
@@ -0,0 +1,28 @@
+/** JS.DOC ==>
+ * -^-^-
+ * @module and @class data_ApiSingleton
+ * -^-^-
+ * @description Singleton Pattern class that manages API 
+ * data, ensuring  a single  instance for global access.
+ * -^-^-
+ */
+  export class data_ApiSingleton {
+    constructor(initialData) {
+
+      // Control block and logic of singleton pattern :
+      if (data_ApiSingleton.instance) return data_ApiSingleton.instance;
+      if (!Array.isArray(initialData)) throw new Error("Data is not an array! data_ApiSingleton expects an array.");
+  
+       // Tools of the singleton :
+      this._data = initialData;
+      data_ApiSingleton.instance = this;
+      console.log("Singleton of recipes initialized!", "\nData of recipes:", this._data);
+    };
+  
+    get data() {
+
+      // Get the datas :
+      return this._data;
+    };
+  };
+  
